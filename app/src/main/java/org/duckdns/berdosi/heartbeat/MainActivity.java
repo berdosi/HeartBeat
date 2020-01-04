eaab151955f85e5affa4ed2cef320f846ac75825
Balint Erdosi <erdosib@gmail.com>
2020-01-04 01:51:56 +0100

    Remove menu
    Signed-off-by: Balint Erdosi <erdosib@gmail.com>

--- app/src/main/java/org/duckdns/berdosi/heartbeat/MainActivity.java
+++ app/src/main/java/org/duckdns/berdosi/heartbeat/MainActivity.java
@@ -11,9 +11,6 @@ import androidx.core.app.ActivityCompat;
 
 import android.view.TextureView;
 import android.view.Surface;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class MainActivity extends AppCompatActivity {
     private final CameraService cameraService = new CameraService(this);
@@ -59,27 +56,5 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.CAMERA},
                 MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
