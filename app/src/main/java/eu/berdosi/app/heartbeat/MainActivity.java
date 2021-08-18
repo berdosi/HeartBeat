3dad87e5525822f48189628e2bf7bebc75c1ab09
dreamsyntax <dreamsyntax@gmail.com>
2021-08-18 14:38:59 -0700

    bugfix: remove stub callback (fixes #24)

--- app/src/main/java/eu/berdosi/app/heartbeat/MainActivity.java
+++ app/src/main/java/eu/berdosi/app/heartbeat/MainActivity.java
@@ -130,7 +130,6 @@ public class MainActivity extends Activity implements ActivityCompat.OnRequestPe
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE_CAMERA) {
             if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                 Snackbar.make(
